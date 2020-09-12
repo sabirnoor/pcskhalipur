68187ae28474691a54e979d9efc813329d44e548
md sabir <sabir@catpl.co.in>
2020-09-12 23:04:34 +0530

    pcs

--- app/Console/Commands/ExamNotice.php
+++ app/Console/Commands/ExamNotice.php
@@ -4,47 +4,46 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class ExamNotice extends Command
-{
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'ExamNotification:cron';
+class ExamNotice extends Command {
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'ExamNotification:cron';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Half yearly exam notice';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Half yearly exam notice';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		parent::__construct();
+	}
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
-        ini_set( 'display_errors', 1 );
-        error_reporting( E_ALL );
-        $from = "developer.kaif@gmail.com";
-        $to = "sibo.sarso@gmail.com";
-        $subject = "PHP Mail Test script";
-        $message = "This is a test to check the cron jobs functionality";
-        $headers = "From:" . $from;
-        mail($to,$subject,$message, $headers);
-        echo "Test email sent PCS";exit;
-    }
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle() {
+
+		ini_set('display_errors', 1);
+		error_reporting(E_ALL);
+		$from = "developer.kaif@gmail.com";
+		$to = "sibo.sarso@gmail.com";
+		$subject = "PHP Mail Test script";
+		$message = "This is a test to check the cron jobs functionality";
+		$headers = "From:" . $from;
+		mail($to, $subject, $message, $headers);
+		//DB::table('test')->delete();
+		echo "Test email sent PCS";exit;
+	}
 }
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,37 +5,37 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
-class Kernel extends ConsoleKernel
-{
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        //
+class Kernel extends ConsoleKernel {
+	/**
+	 * The Artisan commands provided by your application.
+	 *
+	 * @var array
+	 */
+	protected $commands = [
+		//
 		'App\Console\Commands\ExamNotice',
-    ];
+	];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        $schedule->command('inspire')
-                  ->daily();
-    }
+	/**
+	 * Define the application's command schedule.
+	 *
+	 * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+	 * @return void
+	 */
+	protected function schedule(Schedule $schedule) {
+		//$schedule->command('ExamNotification:cron')->everyMinute();
+		$schedule->command('ExamNotification:cron')->weekly()->sundays()->at('07:00');
+		//$schedule->command('inspire')->weekly()->saturdays()->at('22:58');
+		//$schedule->command('ExamNotification:cron')->dailyAt('22:44');
+	}
 
-    /**
-     * Register the Closure based commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
-    {
-        require base_path('routes/console.php');
-    }
+	/**
+	 * Register the Closure based commands for the application.
+	 *
+	 * @return void
+	 */
+	protected function commands() {
+		//$this->load(__DIR__.'/Commands');
+		require base_path('routes/console.php');
+	}
 }
