41ced4a41454a58a98611a48c6f62a8521b08db3
md sabir <sabir@catpl.co.in>
2020-10-02 16:36:01 +0530

    notice

--- app/Console/Commands/ExamNotice.php
+++ app/Console/Commands/ExamNotice.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Quizinvitation;
+use Carbon\Carbon;
 use DB;
 use Illuminate\Console\Command;
 
@@ -41,6 +42,20 @@ class ExamNotice extends Command {
 		} else {
 			$front_url = 'http://pcskhalispur.com/';
 		}
+		$currentDate = Carbon::now('Asia/Kolkata')->toDateString();
+		$allScheduleList = DB::table('quiz_schedule as t')
+			->select(DB::raw('count(t.quiz_id) as totalcunt'), 't.quiz_id', 't.scheduledate', 't2.quiz_title')
+			->join('quiz as t2', 't2.id', '=', 't.quiz_id')
+			->whereDate('t.scheduledate', '=', Carbon::now('Asia/Kolkata')->toDateString())
+			->where('t.IsDelete', 0)->groupBy('t.quiz_id', 't2.quiz_title')->get()->toArray();
+		$quizidArr = [];
+		if ($allScheduleList) {
+			foreach ($allScheduleList as $key => $val) {
+				$quizidArr[$key] = $val->quiz_id;
+			}
+		}
+		pr($quizidArr);
+
 		//ini_set('display_errors', 1);
 		//error_reporting(E_ALL);
 		//$from = "developer.kaif@gmail.com";
@@ -55,21 +70,27 @@ class ExamNotice extends Command {
 			->select('T.id', 'T.quiz_id', 'T.student_master_id', 'T.invitation_link', 'T.isVerified', 'T.sms_sent', 'T2.quiz_start_date', 'T2.quiz_end_date', 'T2.quiz_start_time', 'T2.quiz_end_time', 'T2.quiz_title', 'T3.present_class', 'T3.student_name', 'T3.contact_no')
 			->join('quiz as T2', 'T.quiz_id', '=', 'T2.id', 'LEFT')
 			->join('student_master as T3', 'T.student_master_id', '=', 'T3.id', 'LEFT')
+			->whereIn('T.quiz_id', $quizidArr)
+		//->where(function ($data) use ($quizidArr) {
+		//	if (!empty($quizidArr)) {
+		//		$data->whereIn('T.quiz_id', $quizidArr);
+		//	}
+		//})
 		//->whereDate('T2.quiz_start_date', '>=', Carbon::now('Asia/Kolkata')->toDateString())
 			->where('T.IsDelete', 0)
 			->orderBy('T.created_at', 'ASC')->get()->toArray();
-
+		pr($data);die;
 		if ($data) {
 			foreach ($data as $key => $value) {
 				$startDate = date('dS F', strtotime($value->quiz_start_date));
 				$startTime = date('h:i a', strtotime($value->quiz_start_time));
-				$message = 'Dear students analyse your skill with on line Periodic Test-1 going to start from ' . $startDate . ' from ' . $startTime . '.just one click here ';
+				$message = 'Dear students analyse your skill with on line Term-1 exam going to start from ' . $startDate . ' from ' . $startTime . '.just one click here ';
 				$MsgLink = $front_url . 'din/' . $value->invitation_link;
-				$quizmessage = $message . ' ' . $MsgLink . ' to start the test';
+				$quizmessage = $message . ' ' . $MsgLink . ' to start the exam';
 
 				$msg = str_replace(' ', '%20', $quizmessage);
 				$mobileno = $value->contact_no;
-				echo $url = "http://shikshakiore.com/cpc/isssms.aspx?mobile=$mobileno&msgtxt=$msg&user=INPCSK&lang=english&name=1300";
+				$url = "http://shikshakiore.com/cpc/isssms.aspx?mobile=$mobileno&msgtxt=$msg&user=INPCSK&lang=english&name=1300";
 				die;
 				$ch = curl_init();
 				curl_setopt($ch, CURLOPT_URL, $url);
